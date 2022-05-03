e68a4a38e45e1db32779847ba0f969c0136aafac
borbware <perttu.tuovinen@gmail.com>
2022-05-03 14:38:26 +0300

    add enemypath script that almost works

---
+++ Assets/EnemyPath.cs
@@ -0,0 +1,74 @@
+using Pathfinding;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    public bool canMove;
+    public Transform target;
+
+    public float pickNextWaypointDist = 1.0f;
+    [SerializeField] Vector3 direction;
+
+    Seeker _seeker;
+    Path currentPath;
+    [SerializeField] int nextWaypoint;
+    float speed;
+    bool reachedEndOfPath;
+    void Start()
+    {
+        nextWaypoint = 0;
+        speed = 3;
+        _seeker = GetComponent<Seeker>();
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+    void UpdatePath()
+    {
+        if (_seeker.IsDone())
+            _seeker.StartPath(transform.position, target.position, OnPathCompleted);
+    }
+    void OnPathCompleted(Path p)
+    {
+        Debug.Log("Completed!");
+        if (!p.error)
+        {
+            currentPath = p;
+            nextWaypoint = 0;
+        }
+    }
+    void Update()
+    {
+        if (currentPath == null)
+            return;
+        
+        if (nextWaypoint >= currentPath.vectorPath.Count)
+        {
+            if (!reachedEndOfPath)
+                SendMessage("TargetIsReached");
+            reachedEndOfPath = true;
+            return;
+        } else {
+            reachedEndOfPath = false;
+        }
+
+
+
+        direction = 
+            (currentPath.vectorPath[nextWaypoint] - transform.position)
+            .normalized;
+
+        if (canMove)
+        {
+            transform.position += direction * Time.deltaTime * speed;
+            transform.up = direction;
+
+            float distance = Vector3.Distance(
+                transform.position,
+                currentPath.vectorPath[nextWaypoint]
+            );
+            if (distance < pickNextWaypointDist)
+                nextWaypoint++;
+
+        }
+        
+    }
+}
--- Assets/MoveEnemy.cs
+++ Assets/MoveEnemy.cs
@@ -1,18 +1,14 @@
 using UnityEngine;
-using Pathfinding;
 
 public class MoveEnemy : MonoBehaviour
 {
     [SerializeField] GameObject _player;
-    AIPath _aipath;
-    AIDestinationSetter _aiDestinationSetter;
+    EnemyPath _enemypath;
     public GameObject[] targets = new GameObject[2];
     int targetIndex;
     void Start()
     {
-        _aipath = GetComponent<AIPath>();
-        _aiDestinationSetter = 
-            GetComponent<AIDestinationSetter>();
+        _enemypath = GetComponent<EnemyPath>();
     }
 
     void FollowIfClose()
@@ -21,43 +17,42 @@ public class MoveEnemy : MonoBehaviour
             _player.transform.position, transform.position
             );
         if (dist < 5.0f)
-            _aipath.canMove = true;
+            _enemypath.canMove = true;
         else
-            _aipath.canMove = false;    
+            _enemypath.canMove = false;    
     }
 
     void Update()
     {
         // FollowIfClose();
         if (_player != null 
-        && Vector2.Distance(_player.transform.position, transform.position) < 5.0f 
-        && _aiDestinationSetter != null)
-            _aiDestinationSetter.target = _player.transform;
+        && Vector2.Distance(_player.transform.position, transform.position) < 5.0f)
+            _enemypath.target = _player.transform;
         else if (
             targets.Length > 0 
-            && _aiDestinationSetter != null
             && (
-                _aiDestinationSetter.target == null 
-                || _aiDestinationSetter.target == _player.transform
+                _enemypath.target == null 
+                || _enemypath.target == _player.transform
             ))
         {
             targetIndex = 0;
-            _aiDestinationSetter.target = targets[targetIndex].transform;
+            _enemypath.target = targets[targetIndex].transform;
         }
     }
     public void TargetIsReached()
     {
+        Debug.Log("Reached");
         Invoke("GoToNextTarget",1.0f);
     }
     public void GoToNextTarget()
     {
-        if (_aiDestinationSetter.target != _player.transform)
+        if (_enemypath.target != _player.transform)
         {
             targetIndex += 1;
             targetIndex = targetIndex % targets.Length;
             // if (targetIndex == targets.Length) // same thing in two lines
             //     targetIndex = 0;
-            _aiDestinationSetter.target = targets[targetIndex].transform;
+            _enemypath.target = targets[targetIndex].transform;
         }
     }
     
